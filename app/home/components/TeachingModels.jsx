2543441af5c9cb0e96330163b5fb0c87bdbf73e8
Vahid <vahid.asdfghjkl.moeini@gmail.com>
2025-04-12 09:38:06 +0330

    Fixed again...

--- app/home/components/TeachingModels.jsx
+++ app/home/components/TeachingModels.jsx
@@ -1,7 +1,6 @@
 import { teachingModels } from "@/utils/localData";
 import classes from "./TeachingModels.module.css";
 import Image from "next/image";
-import Link from "next/link";
 import Button from "@/components/ui/Button";
 import SoonText from "@/components/ui/SoonText";
 
@@ -15,78 +14,76 @@ function TeachingModels() {
 
           return (
             <div className={classes.box} key={model.name}>
-              <Link href="">
-                <div className={classes.card}>
-                  <div className={classes.banner}>
-                    <div className={classes.front}>
-                      {model.images.length ? (
-                        <div className={classes.image}>
-                          <Image
-                            src={`/teaching-models/${model.images[0]}`}
-                            fill
-                            alt={model.name}
-                          />
-                        </div>
-                      ) : (
-                        ""
-                      )}
-                      {textIsCentered && (
-                        <h4 className={classes.centeredText}>{model.name}</h4>
-                      )}
-                      {model.comingSoon && (
-                        <div className={classes.soon}>
-                          <SoonText isCentered={true} />
-                        </div>
-                      )}
-                    </div>
-                    <div className={classes.back}>
-                      {model.images.length ? (
-                        <div className={classes.image}>
-                          <Image
-                            src={`/teaching-models/${
-                              model.images.length > 1
-                                ? model.images[1]
-                                : model.images[0]
-                            }`}
-                            fill
-                            alt={model.name}
-                          />
-                        </div>
-                      ) : (
-                        ""
-                      )}
+              <div className={classes.card}>
+                <div className={classes.banner}>
+                  <div className={classes.front}>
+                    {model.images.length ? (
+                      <div className={classes.image}>
+                        <Image
+                          src={`/teaching-models/${model.images[0]}`}
+                          fill
+                          alt={model.name}
+                        />
+                      </div>
+                    ) : (
+                      ""
+                    )}
+                    {textIsCentered && (
+                      <h4 className={classes.centeredText}>{model.name}</h4>
+                    )}
+                    {model.comingSoon && (
+                      <div className={classes.soon}>
+                        <SoonText isCentered={true} />
+                      </div>
+                    )}
+                  </div>
+                  <div className={classes.back}>
+                    {model.images.length ? (
+                      <div className={classes.image}>
+                        <Image
+                          src={`/teaching-models/${
+                            model.images.length > 1
+                              ? model.images[1]
+                              : model.images[0]
+                          }`}
+                          fill
+                          alt={model.name}
+                        />
+                      </div>
+                    ) : (
+                      ""
+                    )}
 
-                      {model.comingSoon ? (
-                        <div className={classes.soon}>
-                          <SoonText isCentered={true} />
-                        </div>
-                      ) : (
-                        <div className={classes.watch}>
-                          <Button color="yellow" roundedSize="sm">
-                            <Image
-                              src="/icons/logout.svg"
-                              width={24}
-                              height={24}
-                              alt="view"
-                            />
-                            مشاهده دوره‌ها
-                          </Button>
+                    {model.comingSoon ? (
+                      <div className={classes.soon}>
+                        <SoonText isCentered={true} />
+                      </div>
+                    ) : (
+                      <div className={classes.watch}>
+                        <Button color="yellow" roundedSize="sm">
                           <Image
-                            src="/icons/play-light.svg"
-                            width={68}
-                            height={68}
-                            alt="play"
+                            src="/icons/logout.svg"
+                            width={24}
+                            height={24}
+                            alt="view"
                           />
-                          <span>تماشای راهنما</span>
-                        </div>
-                      )}
-                    </div>
+                          مشاهده دوره‌ها
+                        </Button>
+                        <Image
+                          src="/icons/play-light.svg"
+                          width={68}
+                          height={68}
+                          alt="play"
+                        />
+                        <span>تماشای راهنما</span>
+                      </div>
+                    )}
                   </div>
                 </div>
-                {!textIsCentered && (
-                  <h4 className={classes.bottomText}>{model.name}</h4>
-                )}
-              </Link>
+              </div>
+              {!textIsCentered && (
+                <h4 className={classes.bottomText}>{model.name}</h4>
+              )}
             </div>
           );
         })}
